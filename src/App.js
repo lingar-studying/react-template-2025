492ac4a2b5768601ca104de092721957f5eed19d
Ido Aharon <ido991155@gmail.com>
2025-01-07 21:13:13 +0200

    adding component+props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component1 } from './component1';
 
 function App() {
   return (
@@ -7,8 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit  <code>src/App.js</code> and save to reload.
         </p>
+        <Component1 name="izhar"/>
+        <Component1 name="ido"/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/component1.jsx
@@ -0,0 +1,7 @@
+export function Component1(props){
+    return(<div>
+      <p>hello world i am Component1</p>
+      <p>hello {props.name}</p>
+      
+    </div>)
+}

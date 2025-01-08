5b6e2928f2adbc1de3ae1ccaf1e085ef579aaa47
Your Name <you@example.com>
2025-01-08 19:00:41 +0200

    problem of render change

--- src/DraftArea.jsx
+++ src/DraftArea.jsx
@@ -1,18 +1,33 @@
 import { Component1 } from "./component1";
 
-export function DraftArea(props){
+export function DraftArea(props) {
+  const car = {
+    company: "mazda",
+    year: 2020,
+    model: 3,
+  };
+  console.log("ido hameleh");
 
-const car={
-    company:"mazda",year: 2020, model: 3
-}
+  car.company="honda";
+  car.year++;
+  car.model="accord"
 
-
-
-
-    return(<div>
+  function changeModel(event){
+   
+    console.log("event=", event, event.target.value);
+    car.model=event.target.value;
+    
+  }
+  return (
+    <div>
       <h1>draft area</h1>
-      <Component1 name="izhar"/>
-      <Component1 name="ido"/>
-      <p>my car= {car.company}, {car.year}, {car.model}</p>
-    </div>)
+      <Component1 name="izhar" />
+      <Component1 name="ido" />
+      <p>
+        my car= {car.company}, {car.year}, {car.model}
+      </p>
+
+      <input type ="text" onChange={changeModel}/>
+    </div>
+  );
 }
